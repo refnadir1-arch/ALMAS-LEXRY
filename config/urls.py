642fa39b9c7d6221119ed0fd892870ef51397668
Nadir Refis <refnadir1@gmaig.com>
2026-04-26 23:26:07 +0100

    clean urls remove temp routes

--- config/urls.py
+++ config/urls.py
@@ -10,22 +10,7 @@ urlpatterns = [
     path("admin-panel/", include("dashboard.urls")),
     path("django-admin/", admin.site.urls),
 ]
-from django.conf import settings
-from django.conf.urls.static import static
+
+# ✅ فقط هذا الجزء يبقى لتشغيل الصور محليًا
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    from orders.views import reset_admin_password
-
-urlpatterns += [
-    path("reset-admin-password/", reset_admin_password),
-]
-from orders.views import create_admin_user
-
-urlpatterns += [
-    path("create-admin-temp/", create_admin_user),
-]
-from orders.views import force_reset_password
-
-urlpatterns += [
-    path("force-reset/", force_reset_password),
-]
